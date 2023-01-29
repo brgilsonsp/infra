a7c822635693680ad56ed1d1f414b5a4371fd2b0
Gilson Souza <br.gilson.sp@gmail.com>
2023-01-29 14:46:01 -0300

    remove docker mssql

--- docker/DB/SqlServer/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-# Run Microsoft SQl Server and initialization script (at the same time)
-/home/db/run-initialization.sh & /opt/mssql/bin/sqlservr
--- docker/DB/SqlServer/run-initialization.sh
+++
@@ -1,4 +0,0 @@
-# Wait to be sure that SQL Server came up
-sleep 90s
-
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'W1n1xStrong(!)Password' -d master -i /home/db/script/scriptbd.sql
